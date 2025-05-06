b308af1df9068b5595bc25ac09fd2c9d3b0c6a46
Beriholic <beriholic@petalmail.com>
2025-05-06 16:01:56 +0800

    feat :sparkles:: use fallout-grub-theme and set docker host in extra init

--- host/virtualisation.nix
+++ host/virtualisation.nix
@@ -24,7 +24,6 @@
   environment.extraInit = ''
     if [ -z "$DOCKER_HOST" -a -n "$XDG_RUNTIME_DIR" ]; then
       export DOCKER_HOST="unix://$XDG_RUNTIME_DIR/podman/podman.sock"
-      set -gx DOCKER_HOST "unix://$XDG_RUNTIME_DIR/podman/podman.sock"
     fi
   '';
 }
